183d35f6862bc215ad29c3bcd600c04dd96ee5d6
jojosuelobo <josue.lobo@etaure.com.br>
2023-09-25 15:36:57 -0300

    increased defaultCommandTimeout

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://revamp-ps.etaure.com.br:9000',
-    defaultCommandTimeout: 5000,
+    defaultCommandTimeout: 8000,
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
     // },
